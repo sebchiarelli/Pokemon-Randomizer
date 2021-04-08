05e3b241daf8c45c35a51807b3933ecfc3b834d2
Sébastien <@none.com>
2021-04-08 19:55:50 +0200

    ajout d'une api+changement de theme de fond

--- chooseYou.js
+++ chooseYou.js
@@ -1,26 +1,47 @@
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
-const whichOne = getRandomInt(898);
+const pokemon = getRandomInt(898);
 const picture = document.createElement('img');
 
 const div = document.querySelector("div");
 const p = document.querySelectorAll("p");
 
 const moveSet = document.createElement('span');
-const talent = getRandomInt(267);
+//const talent = getRandomInt(267);
 const moveOne = getRandomInt(830);
 const moveTwo = getRandomInt(830);
 const moveThree = getRandomInt(830);
 const moveFour = getRandomInt(830);
 const moveStab = getRandomInt(191);
 const shiny = getRandomInt(259);
+//const updateTalent = () => {document.createElement("p");}
+
 
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max))+1;
 }
 
-picture.src = `${baseURL}${whichOne}.png`;
+picture.src = `${baseURL}${pokemon}.png`;
 div.prepend(picture);
 
 
-p[0].appendChild(moveSet);
+
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+.then(res => {
+    
+    return res.json()
+})
+.then(data => {
+    console.log("now the data", data);
+    console.log(data.abilities[0].ability.name);
+    const talent = data.abilities[0].ability.name;
+    console.log(talent);
+    const updateTalent = document.createElement("span");
+    p[0].appendChild(updateTalent);
+    updateTalent.innerText = talent;
+})
+.catch(e =>{
+    console.log(e)
+})
+
