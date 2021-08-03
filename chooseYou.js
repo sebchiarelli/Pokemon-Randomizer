d3b38e9a1546a32208035f1adeb9ab1d1d1d7aa9
Sébastien <@none.com>
2021-08-03 15:24:59 +0200

    abilities are translated and link to their page on pokepedia. Pokemon name send to the pokemon's page on pokepedia also

--- chooseYou.js
+++ chooseYou.js
@@ -1,10 +1,26 @@
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 const pokemonNumber = getRandomInt(898);
 const picture = document.createElement('img');
-const createSpan = document.createElement('span');
+const createA = document.createElement('a');
 const div = document.querySelector('div');
 const p = document.querySelectorAll('p');
 const button = document.querySelectorAll('button');
+
+let langue = 0;
+let langueFound = false;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max))+1;
+};
+
+function langueFr(data){
+    langue = 0;
+    langueFound = false;
+    do{
+        if(data.names[langue].language.name === "fr"){langueFound = true;}
+        else{langue++; }
+    } while (langueFound === false);
+} 
 /*
 const moveSet = document.createElement('span');
 const moveOne = getRandomInt(826);
@@ -14,25 +30,23 @@ const moveFour = getRandomInt(826);
 const moveStab = getRandomInt(191);
 const shiny = getRandomInt(259);
 */
+
+//Name of the random pokemon in french
 const pokemonName = fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonNumber}`)
 .then(res => {return res.json()})
 .then(data => {
-    let langue = 0;
-    if(data.names[4].language.name === "fr"){
-        langue = 4
-    }
-    else{ langue = 3};
-    
-    createSpan.innerHTML = data.names[langue].name.toUpperCase()
+    langueFr(data);
+    createA.target = "_blank";
+    createA.href = `https://www.pokepedia.fr/${data.names[langue].name}`;
+    createA.innerHTML = data.names[langue].name.toUpperCase()
 })
 .catch(e =>{console.log(e)});
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max))+1;
-};
+div.prepend(createA);
 
-div.prepend(createSpan);
+
 picture.src = `${baseURL}${pokemonNumber}.png`;
+picture.classList.add("pic");
 div.prepend(picture);
 
 
@@ -55,17 +69,35 @@ button[1].addEventListener('click', function(){
 })
 
 button[0].addEventListener('click', function(){
-    fetch(`https://pokeapi.co/api/v2/ability?offset=0&limit=327`)
+    fetch(`https://pokeapi.co/api/v2/ability?offset=0&limit=267`)
     .then(res => {
         
         return res.json()
     })
     .then(data => {
-        const talentNumber = getRandomInt(328)-1;
-        const talent = data.results[talentNumber].name;
-        const updateTalent = document.createElement("span");
-        p[0].appendChild(updateTalent);
-        updateTalent.innerText = talent;
+        const talentNumber = getRandomInt(267)-1;
+        
+        fetch(`https://pokeapi.co/api/v2/ability/${talentNumber}/`)
+        .then(res => {
+        
+        return res.json()
+        })
+        .then(data => {
+            
+            langueFr(data);
+            const talent = data.names[langue].name;
+            const updateTalent = document.createElement("a");
+            updateTalent.target = "_blank";
+            updateTalent.href = `https://www.pokepedia.fr/${talent}`;
+            p[0].appendChild(updateTalent);
+            updateTalent.innerText = talent + " ";
+        })
+        .catch(e =>{
+        console.log(e)
+        })
+        
+        //const talent = data.results[talentNumber].name;
+       
     })
     .catch(e =>{
         console.log(e)
